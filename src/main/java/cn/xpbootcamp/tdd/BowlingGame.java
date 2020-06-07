9f08635c1ec29687e520e1273df36a3bd78c2e88
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-06-07 10:48:34 +0800

    I.3.1 Extract query method isSpare
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cn/xpbootcamp/tdd/BowlingGame.java
+++ src/main/java/cn/xpbootcamp/tdd/BowlingGame.java
@@ -14,16 +14,19 @@ public class BowlingGame {
         int totalScore = 0;
         int rollIndex = 0;
         for (int round = 0; round < 10; round++) {
-            if (pouredNumbers.get(rollIndex) + pouredNumbers.get(rollIndex + 1) == 10) {
+            if (isSpare(rollIndex)) {
                 totalScore += 10;
                 totalScore += pouredNumbers.get(rollIndex + 2);
-                rollIndex += 2;
             } else {
                 totalScore += pouredNumbers.get(rollIndex);
                 totalScore += pouredNumbers.get(rollIndex + 1);
-                rollIndex += 2;
             }
+            rollIndex += 2;
         }
         return totalScore;
+    }
+
+    private boolean isSpare(int rollIndex) {
+        return pouredNumbers.get(rollIndex) + pouredNumbers.get(rollIndex + 1) == 10;
     }
 }
