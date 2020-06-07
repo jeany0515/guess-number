c3cf54eaa3b38f83aac3a44836c95d75797aa5ae
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-06-07 10:39:16 +0800

    I.2.1 Remove duplicated method in Test
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/test/java/cn/xpbootcamp/tdd/BowlingGameTest.java
+++ src/test/java/cn/xpbootcamp/tdd/BowlingGameTest.java
@@ -1,30 +1,33 @@
 package cn.xpbootcamp.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BowlingGameTest {
+    private BowlingGame bowlingGame;
+
+    @BeforeEach
+    void setup() {
+        bowlingGame = new BowlingGame();
+    }
 
     @Test
     void should_return_0_when_scoring_given_every_roll_is_0() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        for (int rollIndex = 0; rollIndex < 20; rollIndex++) {
-            bowlingGame.roll(0);
-        }
-
+        rolls(0);
         assertThat(bowlingGame.scoring()).isEqualTo(0);
     }
 
     @Test
     void should_sum_all_rolls_when_scoring_given_every_roll_is_common_as_3() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        for (int rollIndex = 0; rollIndex < 20; rollIndex++) {
-            bowlingGame.roll(3);
-        }
-
+        rolls(3);
         assertThat(bowlingGame.scoring()).isEqualTo(60);
+    }
+
+    private void rolls(int score) {
+        for (int rollIndex = 0; rollIndex < 20; rollIndex++) {
+            bowlingGame.roll(score);
+        }
     }
 }
